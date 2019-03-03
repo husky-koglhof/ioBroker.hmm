e01953c06499dde2e312d84bd869b5cb47a7fa3d
Ingo Fischer <github@fischer-ka.de>
2019-03-03 22:43:58 +0100

    add adapter-core

--- hmm.js
+++ hmm.js
@@ -11,7 +11,7 @@
 /*jslint node: true */
 "use strict";
 
-var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
+var utils = require('@iobroker/adapter-core'); // Get common adapter utils
 
 var adapter = utils.Adapter({
     name:  'hmm',
